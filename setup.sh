294e8b34f68abed1f6c366eb2297bad92cbb5c21
Chris Ackerman <chris@topher.land>
2024-09-16 20:14:20 +0000

    update

--- setup.sh
+++ setup.sh
@@ -15,8 +15,11 @@ sudo -iu steam bash <<'EOF'
 cd ~
 set -ex
 /usr/games/steamcmd +@sSteamCmdForcePlatformType linux +force_install_dir "$HOME/valheimserver" +login anonymous +app_update 896660 +quit
-mkdir -p "$HOME/valheimserver/valheim_save_data"
-echo 76561197994236995 > "$HOME/valheimserver/valheim_save_data/adminlist.txt"
+if [ ! -d "$HOME/valheimserver/valheim_save_data" ]; then 
+  mkdir -p "$HOME/valheimserver/valheim_save_data"
+  echo 76561197994236995 > "$HOME/valheimserver/valheim_save_data/adminlist.txt"
+  echo 76561197994236995 > "$HOME/valheimserver/valheim_save_data/permittedlist.txt"
+fi
 EOF
 
 #ln -s valheimserver.service /etc/systemd/system/valheimserver.service
